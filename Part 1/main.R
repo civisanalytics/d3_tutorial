55593f7ae7b39e1ba896aa388f0a2e67479eac30
pcooman <pcooman@civisanalytics.com>
2019-02-25 20:09:30 -0600

    part 1

---
+++ Part 1/main.R
@@ -0,0 +1,32 @@
+library(readr)
+library(jsonlite)
+
+url <- "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-01-08/IMDb_Economist_tv_ratings.csv"
+
+df <-read.csv(url(url))
+
+df_json <- jsonlite::toJSON(df)
+
+header <- read_file("header.txt")
+footer <- read_file("footer.txt")
+script <- paste0(header,
+                 df_json,
+                 footer)
+
+# To show in browser
+fileConn <- file('index.html')
+writeLines(script, fileConn)
+close(fileConn)
+viewer('index.html')
+
+# To show in Viewer Pane
+tempDir <- tempfile() # --> this is key!
+dir.create(tempDir)
+htmlFile <- file.path(tempDir, "index.html")
+
+fileConn <- file(htmlFile)
+writeLines(script, fileConn)
+close(fileConn)
+
+viewer <- getOption("viewer")
+viewer(htmlFile)
